3a659bdc73c23f54f344f730239050865416a01d
Vladimir Afanasiev <vladimir.afanasiev@scalewill.com>
2023-07-07 00:02:56 +0300

    fix test

--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -4,36 +4,24 @@ require 'test_helper'
 
 User = Struct.new(:name, :job, keyword_init: true)
 
-describe 'Assertions' do
-  it 'test form_for with class' do
-    user = User.new job: 'hexlet'
-
-    result = HexletCode.form_for user, url: '#' do |f|
-      f.input :name, class: 'user-input'
-    end
-
-    expected = "<form action='#' method='post'>" \
-               "<label for='name'>Name</label>" \
-               "<input name='name' type='text' value='' class='user-input'>" \
-               '</form>'
-
-    assert_equal expected, result
-  end
-
+describe 'Assertions1' do
   it 'test form_for with submit' do
     user = User.new job: 'hexlet'
 
     result = HexletCode.form_for user do |f|
-      f.input :job, as: :text
+      f.input :job, as: :text, class: 'active'
     end
 
     expected = "<form action='#' method='post'>" \
                "<label for='job'>Job</label>" \
-               "<textarea name='job' cols='20' rows='40'>hexlet</textarea>" \
+               "<textarea name='job' cols='20' rows='40' class='active'>hexlet</textarea>" \
                '</form>'
 
     assert_equal expected, result
   end
+end
+
+describe 'Assertions2' do
   it 'test form_for with submit' do
     user = User.new job: 'hexlet'
 
